148b0811e5379f61abdadcf40d3d0f683a15d081
Walter A. Boring IV <waboring@hemna.com>
2017-06-22 11:37:41 -0700

    another add

--- weewx_hemna_plugin/hemna.py
+++ weewx_hemna_plugin/hemna.py
@@ -1,4 +1,3 @@
-import datetime
 import re
 import sys
 import syslog
@@ -9,7 +8,7 @@ import urllib2
 
 import weewx
 from weewx import restx
-from weeutil import weeutil
+# from weeutil import weeutil
 
 
 class StdHemna(restx.StdRESTful):
@@ -24,11 +23,13 @@ class StdHemna(restx.StdRESTful):
         _manager_dict = weewx.manager.get_manager_dict_from_config(
             config_dict, 'wx_binding')
 
-        _ambient_dict = restx.get_site_dict(config_dict, 'Hemna', 'station', 'password')
+        _ambient_dict = restx.get_site_dict(config_dict, 'Hemna', 'station',
+                                            'password')
         _ambient_dict.setdefault('server_url', self.archive_url)
 
         self.archive_thread = HemnaThread(
-            self.archive_queue, _manager_dict, protocol_name="Hemna", **_ambient_dict)
+            self.archive_queue, _manager_dict,
+            protocol_name="Hemna", **_ambient_dict)
         self.archive_thread.start()
         self.bind(weewx.NEW_ARCHIVE_RECORD, self.new_archive_record)
 
@@ -47,33 +48,33 @@ class StdHemna(restx.StdRESTful):
     def new_loop_packet(self, event):
         syslog.syslog(syslog.LOG_DEBUG, "hemna: raw packet: %s" % event.packet)
         self.cached_values.update(event.packet, event.packet['dateTime'])
-        syslog.syslog(syslog.LOG_DEBUG, "hemna: cached packet: %s" % 
-            self.cached_values.get_packet(event.packet['dateTime']))
+        syslog.syslog(syslog.LOG_DEBUG, "hemna: cached packet: %s" %
+                      self.cached_values.get_packet(event.packet['dateTime']))
         self.loop_queue.put(
             self.cached_values.get_packet(event.packet['dateTime']))
 
-class HemnaThread(restx.RESTThread):
-    
-    # Types and formats of the data to be published:
-    _FORMATS = {'dateTime'    : 'datetime=%s',
-                'barometer'   : 'rel_pressure=%.3f',
-                'outTemp'     : 'temp_out=%.1f',
-                'inTemp'      : 'temp_in=%.1f',
-                'outHumidity' : 'rel_hum_out=%.0f',
-                'inHumidity'  : 'rel_hum_in=%.0f',
-                'windSpeed'   : 'wind_speed=%.1f',
-                'windDir'     : 'wind_angle=%.0f',
-                'windchill'   : 'wind_chill=%.1f',
-                'dewpoint'    : 'dewpoint=%.1f',
-                'rainTotal'   : 'rain_total=%.2f',
-                'hourRain'    : 'rain_1h=%.2f',
-                'dayRain'     : 'rain_24h=%.2f'}
 
+class HemnaThread(restx.RESTThread):
+
+    # Types and formats of the data to be published:
+    _FORMATS = {'dateTime': 'datetime=%s',
+                'barometer': 'rel_pressure=%.3f',
+                'outTemp': 'temp_out=%.1f',
+                'inTemp': 'temp_in=%.1f',
+                'outHumidity': 'rel_hum_out=%.0f',
+                'inHumidity': 'rel_hum_in=%.0f',
+                'windSpeed': 'wind_speed=%.1f',
+                'windDir': 'wind_angle=%.0f',
+                'windchill': 'wind_chill=%.1f',
+                'dewpoint': 'dewpoint=%.1f',
+                'rainTotal': 'rain_total=%.2f',
+                'hourRain': 'rain_1h=%.2f',
+                'dayRain': 'rain_24h=%.2f'}
 
     def __init__(self, queue, manager_dict, station, password, server_url,
-                 protocol_name="Hemna", post_interval=None, max_backlog=sys.maxint,
-                 stale=None, log_success=True, log_failure=True, 
-                 timeout=10, max_tries=3, retry_wait=5,
+                 protocol_name="Hemna", post_interval=None,
+                 max_backlog=sys.maxint, stale=None, log_success=True,
+                 log_failure=True, timeout=10, max_tries=3, retry_wait=5,
                  softwaretype='weewx-%s' % weewx.__version__,
                  skip_upload=False):
 
@@ -95,7 +96,7 @@ class HemnaThread(restx.RESTThread):
         self.password = password
         self.server_url = server_url
         self.formats = HemnaThread._FORMATS
-    
+
     def format_url(self, record):
         """Return an URL for posting using WOW's version of the Ambient
         protocol."""
@@ -109,8 +110,9 @@ class HemnaThread(restx.RESTThread):
             # Check to make sure the type is not null
             if _v is not None:
                 if _key == 'dateTime':
-                    #_v = urllib.quote_plus(datetime.datetime.utcfromtimestamp(_v).isoformat(' '))
-                    #_v = urllib.quote_plus(weeutil.timestamp_to_string(_v))
+                    # _v = urllib.quote_plus(
+                    # datetime.datetime.utcfromtimestamp(_v).isoformat(' '))
+                    # _v = urllib.quote_plus(weeutil.timestamp_to_string(_v))
                     format_str = "%Y-%m-%d %H:%M:%S"
                     date_str = time.strftime(format_str, time.localtime(_v))
                     _v = urllib.quote_plus(date_str)
@@ -118,18 +120,16 @@ class HemnaThread(restx.RESTThread):
                 _liststr.append(HemnaThread._FORMATS[_key] % _v)
             else:
                 _liststr.append(HemnaThread._FORMATS[_key] % float(0))
-   
 
         # wind angle
         _liststr.append("wind_direction=N")
-        #T = record.get('outTemp')
-        #V = record.get('windSpeed')
-        #wind_chill = 35.74 + (0.6215*T) - 35.75*(V**0.16) + 0.4275*T*(V**0.16)
-        #_liststr.append("wind_chill=%.1f" % wind_chill)
-        #_liststr.append("rain_total=0.00")
+        # T = record.get('outTemp')
+        # V = record.get('windSpeed')
+        # wind_chill = 35.74 + (0.6215*T) - 35.75*(V**0.16) +0.4275*T*(V**0.16)
+        # _liststr.append("wind_chill=%.1f" % wind_chill)
+        # _liststr.append("rain_total=0.00")
         _liststr.append("tendency=na")
         _liststr.append("forecast=na")
-
 
         # Now stick all the pieces together with an ampersand between them:
         _urlquery = '&'.join(_liststr)
@@ -137,7 +137,7 @@ class HemnaThread(restx.RESTThread):
         _url = "%s?%s" % (self.server_url, _urlquery)
         # show the url in the logs for debug, but mask any password
         if weewx.debug >= 2:
-            syslog.syslog(syslog.LOG_DEBUG, "restx: HEMNA: url: %s" % 
+            syslog.syslog(syslog.LOG_DEBUG, "restx: HEMNA: url: %s" %
                           re.sub(r"siteAuthenticationKey=[^\&]*",
                                  "siteAuthenticationKey=XXX", _url))
         return _url
@@ -150,12 +150,11 @@ class HemnaThread(restx.RESTThread):
                 _response = urllib2.urlopen(request, timeout=self.timeout)
             except TypeError:
                 _response = urllib2.urlopen(request)
-        except urllib2.HTTPError, e:
+        except urllib2.HTTPError as e:
             # WOW signals a bad login with a HTML Error 400 or 403 code:
             if e.code == 400 or e.code == 403:
-                raise BadLogin(e)
+                raise restx.BadLogin(e)
             else:
                 raise
         else:
             return _response
-
